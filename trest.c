ba3ab5b913b161970d7774385a60ecd69dbabd33
Alexander Sack <asac@pantacor.com>
2016-10-04 15:12:28 +0200

    major code landing to implement trest_update_auth in trest.c

--- trest.c
+++ trest.c
@@ -38,13 +38,140 @@ struct trest_request {
 
 	char *endpoint_path;
 	char **queries;
+	char **headers;
 	char *json_body;
 };
 
+static int
+parse_json (const char *buf, jsmntok_t **jsonv_out, int *jsons_out)
+{
+	jsmn_parser parser;
+	int r;
+
+	jsmn_init (&parser);
+
+	*jsons_out=10;
+	*jsonv_out = malloc (*jsons_out * sizeof(jsmntok_t));
+
+	if (*jsonv_out == NULL) {
+		fprintf(stderr, "malloc(): errno=%d\n", errno);
+		return 0;
+	}
+again:
+	r = jsmn_parse(&parser, buf, strlen (buf), *jsonv_out,
+		       *jsons_out);
+
+	if (r < 0) {
+		if (r == JSMN_ERROR_NOMEM) {
+			*jsons_out = *jsons_out * 2;
+			*jsonv_out = realloc(*jsonv_out, sizeof(jsmntok_t)
+					     * *jsons_out);
+			if (jsonv_out == NULL) {
+				return 0;
+			}
+			goto again;
+		}
+	}
+	return r;
+}
+
+static void
+update_tokens_from_json_response (struct trest *self,
+				  struct trest_response *response)
+{
+	// if we got a response parse it
+	jsmntok_t *jv;
+	int c,i,t=-1;
+	char *body;
+
+	jv = response->json_tokv;
+	c = response->json_tokc;
+	body = response->body;
+
+	for (i=0; i<c && t != 0; i++) {
+		int n = jv[i].end - jv[i].start;
+		char *token = malloc(n+1);
+		strncpy(token, body + jv[i].start, n);
+		token[n]=0;
+
+		if (jv[i].type == JSMN_STRING
+		    && !strncmp(body + jv[i].start, "token", 5)) {
+			t=1;
+		} else if (t==1 && jv[i].type == JSMN_STRING) {
+			token[n] = 0;
+			strncpy(token, body + jv[i].start, n);
+			if (self->access_token)
+				free(self->access_token);
+			self->access_token = strdup(token);
+
+			if (self->refresh_token)
+				free(self->refresh_token);
+			self->refresh_token = strdup (token);
+		} else if (t==1) {
+			t=0;
+		}
+		free(token);
+	}
+}
+
+static trest_auth_status_enum
+do_refresh_login (struct trest *self)
+{
+	return TREST_AUTH_STATUS_UNKNOWN;
+}
+
+static trest_auth_status_enum
+do_credentials_login (struct trest *self)
+{
+	trest_response_ptr r;
+	trest_request_ptr p;
+
+	const char* userpass =
+		"{ \"username\": \"%s\""
+		", \"password\": \"%s\" }";
+
+	char *b = malloc (sizeof(char) *
+			  (strlen(userpass)
+			   + strlen(self->credentials[0])
+			   + strlen(self->credentials[1])));
+
+	int rv = sprintf(b, userpass, self->credentials[0], self->credentials[1]);
+
+	if (DEBUG)
+		printf ("do_credentials_login with body : %s\n", b);
+
+	p = trest_make_request (TREST_METHOD_POST,
+				"/api/auth/login",
+				NULL,
+				NULL,
+				b);
+	free (b);
+
+	r = trest_do_json_request (self, p);
+	trest_request_free(p);
+
+	if (DEBUG)
+		printf ("do_credentials_login code: %d\n", r->code);
+	if (r->code == THTTP_STATUS_UNAUTHORIZED) {
+		self->status = TREST_AUTH_STATUS_NOTAUTH;
+		goto exit;
+	} else if (r->code != THTTP_STATUS_OK) {
+		self->status = TREST_AUTH_STATUS_ERROR;
+		goto exit;
+	}
+	update_tokens_from_json_response (self, r);
+
+	self->status = TREST_AUTH_STATUS_OK;
+exit:
+	trest_response_free(r);
+	return self->status;
+}
+
+
 trest_ptr
 trest_new_from_userpass(const char* host, int port,
-				const char *user,
-				const char *pass)
+			const char *user,
+			const char *pass)
 {
 	struct trest* client =
 		calloc (sizeof(struct trest), 1);
@@ -72,6 +199,10 @@ trest_free (trest_ptr ptr)
 		free (*cptr++);
 	}
 
+	if (client->access_token)
+		free(client->access_token);
+	if (client->refresh_token)
+		free(client->refresh_token);
 	free(client->credentials);
 	free(client->host);
 	free (ptr);
@@ -115,6 +246,7 @@ trest_auth_status (trest_ptr ptr)
 	return self->status;
 }
 
+
 // update auth tokens if possible. usually tries refresh_token
 // against the login endpoint and then other credentials
 // if available. usually needs to be called once to get
@@ -123,8 +255,15 @@ trest_auth_status (trest_ptr ptr)
 trest_auth_status_enum
 trest_update_auth (trest_ptr ptr)
 {
-
 	struct trest *self = (struct trest*) ptr;
+
+	// if we believe we are authed, use refresh logic...
+	if (trest_auth_status (ptr) == TREST_AUTH_STATUS_OK) {
+		return do_refresh_login (self);
+	} else {
+		return do_credentials_login (self);
+	}
+
 	return TREST_AUTH_STATUS_UNKNOWN;
 }
 
@@ -132,37 +271,62 @@ trest_update_auth (trest_ptr ptr)
 // and Accept-Endcoding application/json accordingly
 trest_request_ptr
 trest_make_request (trest_method_enum method,
-		char *endpoint_path,
-		char **queries,
-		char *json_body)
+		    char *endpoint_path,
+		    char **queries,
+		    char **headers,
+		    char *json_body)
 {
 	// XXX: implement parameter precondition checks
 	struct trest_request* r =
 		malloc (sizeof(struct trest_request));
 
 	char **queries_i = queries;
+	char **headers_i = headers;
 
 	r->method = method;
 	r->endpoint_path = strdup(endpoint_path);
-	r->json_body= strdup(json_body);
+	r->json_body = strdup(json_body);
 	r->queries = 0;
+	r->headers = 0;
 
-	if (queries_i) {
-		size_t queries_size = 1;
-		int queries_len = 0;
-		r->queries = calloc(sizeof(char*), queries_size);
-		r->queries[queries_len] = 0;
+	if (!queries_i)
+		goto headers;
 
-		// XXX: make a ptrvdup
-		while (*queries_i) {
-			r->queries[queries_len] = strdup(*queries_i++);
-			queries_len++;
-			if (queries_len >= queries_size) {
-				queries_size += 128 * sizeof(char*);
-				r->queries = realloc(r->queries, queries_size);
-			}
+	size_t queries_size = 1;
+	int queries_len = 0;
+	r->queries = calloc(sizeof(char*), queries_size);
+	r->queries[queries_len] = 0;
+
+	// XXX: make a ptrvdup
+	while (*queries_i) {
+		r->queries[queries_len] = strdup(*queries_i++);
+		queries_len++;
+		if (queries_len >= queries_size) {
+			queries_size += 128 * sizeof(char*);
+			r->queries = realloc(r->queries, queries_size);
 		}
 	}
+
+headers:
+	if (!headers_i)
+		goto exit;
+
+	size_t headers_size = 1;
+	int headers_len = 0;
+	r->headers = calloc(sizeof(char*), headers_size);
+	r->headers[headers_len] = 0;
+
+	// XXX: make a ptrvdup
+	while (*headers_i) {
+		r->headers[headers_len] = strdup(*headers_i++);
+		headers_len++;
+		if (headers_len >= headers_size) {
+			headers_size += 128 * sizeof(char*);
+			r->headers = realloc(r->headers, headers_size);
+		}
+	}
+
+exit:
 	return r;
 }
 
@@ -175,51 +339,20 @@ trest_make_request (trest_method_enum method,
 //   -- default the client uses.
 trest_response_ptr
 trest_do_request (trest_ptr client,
-		trest_request_ptr request,
-		trest_cb callback,
-		void* user_data)
+		  trest_request_ptr request,
+		  trest_cb callback,
+		  void* user_data)
 {
 	return NULL;
 }
 
-static int
-parse_json (const char *buf, jsmntok_t **jsonv_out, int *jsonc_out)
-{
-	jsmn_parser parser;
-	int r;
-
-	jsmn_init (&parser);
-
-	*jsonc_out=10;
-	*jsonv_out = malloc (*jsonc_out * sizeof(jsmntok_t));
-
-	if (*jsonv_out == NULL) {
-		fprintf(stderr, "malloc(): errno=%d\n", errno);
-		return 0;
-	}
-again:
-	r = jsmn_parse(&parser, buf, strlen (buf), *jsonv_out,
-		*jsonc_out);
-
-	if (r < 0) {
-		if (r == JSMN_ERROR_NOMEM) {
-			*jsonc_out = *jsonc_out * 2;
-			*jsonv_out = realloc(*jsonv_out, sizeof(jsmntok_t)
-					* *jsonc_out);
-			if (jsonv_out == NULL) {
-				return 0;
-			}
-			goto again;
-		}
-	}
-	return *jsonc_out;
-}
 
 trest_response_ptr
 trest_do_json_request (trest_ptr client,
-			trest_request_ptr request)
+		       trest_request_ptr request)
 {
 
+	t_thttp_response *response;
 	t_thttp_request *req = thttp_request_new_0();
 	trest_response_t *res = calloc (sizeof(trest_response_t),1);
 	struct trest_request *req_in = (struct trest_request*) request;
@@ -234,19 +367,37 @@ trest_do_json_request (trest_ptr client,
 	req->path = req_in->endpoint_path;
 	req->host = c->host;
 	req->port = c->port;
-	req->body = 0;
-	req->headers = 0;
+	req->body = req_in->json_body;
+	if (req_in->json_body)
+		req->body_content_type = "application/json";
 
-	t_thttp_response *response = thttp_request_do(req);
-	res->body = strdup(response->body);
+	response = thttp_request_do(req);
+
+	if (response->body) {
+		int jc;
+		res->body = strdup(response->body);
+		res->json_tokc = parse_json (response->body, &res->json_tokv, &res->json_toks);
+		if (!res->json_tokc)
+		{
+			// XXX: update auth status of response?
+			printf ("failed to parse_json\n");
+			trest_response_free(res);
+			res = 0;
+		}
+	}
+	// XXX: strvdup this one...
 	res->headers = response->headers;
+	res->code = response->code;
 
-	if (!parse_json (response->body, &res->json_tokv, &res->json_tokc))
-	{
-		// XXX: update auth status of response?
-		printf ("failed to parse_json\n");
-		trest_response_free(res);
-		res = 0;
+	switch (response->code) {
+	case THTTP_STATUS_UNAUTHORIZED:
+		res->status = TREST_AUTH_STATUS_NOTAUTH;
+	case THTTP_STATUS_OK:
+		res->status = TREST_AUTH_STATUS_OK;
+	default:
+		res->status = TREST_AUTH_STATUS_ERROR;
+		// XXX: this needs to be redone in a way that guides
+		// clients on how to solve...
 	}
 
 	thttp_request_free(req);
@@ -263,5 +414,5 @@ trest_do_json_request (trest_ptr client,
 //  - EOF:  data == 0 && data_len == 0
 //  - ERROR" data == 0 && data_len == ERROR_CODE
 typedef void (*trest_cb) (void *user_data,
-			unsigned char* data,
-			size_t data_len);
+			  unsigned char* data,
+			  size_t data_len);
