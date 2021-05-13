15b29495b23c5c40b353e3f6515cf63ca4512c58
Alexander Sack <asac@pantacor.com>
2021-05-13 20:58:22 +0200

    revive thttp examples; add as separate binaries to atom.mk

---
+++ thttp-example1-tls.c
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2017 Pantacor Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "jsmn/jsmn.h"
+#include "thttp.h"
+
+static int
+dump_json (const char *js, jsmntok_t *t, size_t count, int indent) {
+	int i, j, k;
+	if (count == 0) {
+		return 0;
+	}
+	if (t->type == JSMN_PRIMITIVE) {
+		printf("%.*s", t->end - t->start, js+t->start);
+		return 1;
+	} else if (t->type == JSMN_STRING) {
+		printf("'%.*s'", t->end - t->start, js+t->start);
+		return 1;
+	} else if (t->type == JSMN_OBJECT) {
+		printf("\n");
+		j = 0;
+		for (i = 0; i < t->size; i++) {
+			for (k = 0; k < indent; k++) printf("  ");
+			j += dump_json (js, t+1+j, count-j, indent+1);
+			printf(": ");
+			j += dump_json(js, t+1+j, count-j, indent+1);
+			printf("\n");
+		}
+		return j+1;
+	} else if (t->type == JSMN_ARRAY) {
+		j = 0;
+		printf("\n");
+		for (i = 0; i < t->size; i++) {
+			for (k = 0; k < indent-1; k++) printf("  ");
+			printf("   - ");
+			j += dump_json(js, t+1+j, count-j, indent+1);
+			printf("\n");
+		}
+		return j+1;
+	}
+	return 0;
+}
+
+
+
+int main (char **argv, int argc) {
+
+	thttp_request_t* req = (thttp_request_t*) thttp_request_tls_new_0 ();
+	thttp_response_t* res = 0;
+	int r;
+
+	req->host_proxy = NULL;
+	req->port_proxy = 3128;
+
+	
+	req->method = THTTP_METHOD_POST;
+	req->proto = THTTP_PROTO_HTTP;
+	req->proto_version = THTTP_PROTO_VERSION_10;
+	req->user_agent = "Libthttp Test/1.0";
+	req->host = "api.pantahub.com";
+	req->port = 443;
+	req->path = "/auth/login";
+	req->headers = 0;
+	req->body =
+		"{"
+		"	\"username\": \"user1\","
+		"	\"password\": \"user1\""
+		"}";
+	req->body_content_type = "application/json";
+
+	res = thttp_request_do (req);
+
+	printf("\nResult Retrieved:\n%s\n", res->body);
+
+
+	int tokcount=10;
+	jsmntok_t *tok;
+	jsmn_parser parser;
+	jsmn_init (&parser);
+
+	tok = malloc (tokcount * sizeof(*tok));
+
+	if (tok == NULL) {
+		fprintf(stderr, "malloc(): errno=%d\n", errno);
+		return 3;
+	}
+
+
+again:
+	r = jsmn_parse(&parser, res->body, strlen (res->body), tok, tokcount);
+	if (r < 0) {
+		if (r == JSMN_ERROR_NOMEM) {
+			tokcount = tokcount * 2;
+			tok = realloc(tok, sizeof(*tok) * tokcount);
+			if (tok == NULL) {
+				return 3;
+			}
+			goto again;
+		}
+	} else {
+		dump_json (res->body, tok, parser.toknext, 0);
+	}
+	free(tok);
+
+	thttp_request_free(req);
+	thttp_response_free(res);
+
+	req = thttp_request_new_0 ();
+
+	req->method = THTTP_METHOD_GET;
+	req->proto = THTTP_PROTO_HTTP;
+	req->proto_version = THTTP_PROTO_VERSION_10;
+	req->host = "api.pantahub.com";
+	req->port = 443;
+	req->path = "/local-s3/51d24ff501bbb666ca0625514577440696b7ccb7f7c8fcba7a04b421f9ae86a1";
+	req->headers = 0;
+	req->body = "";
+	req->body_content_type = "";
+
+	int fd;
+	char name[] = "/tmp/fileXXXXXX";
+	fd = mkstemp(name);
+
+	res = thttp_request_do_file (req, fd);
+
+	close (fd);
+	printf("\nResult Retrieved in:\n%s\n", name);
+
+	thttp_request_free(req);
+	thttp_response_free(res);
+
+	return 0;
+}
